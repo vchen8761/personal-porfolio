3fef9316ec4a70ad0c644568369e5b1f19bc2eb4
vchen8761 <vchen8761@gmail.com>
2020-07-06 17:19:12 -0400

    added build

---
+++ build/precache-manifest.9c5b48c39367a39c89e5767851bcd9a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7f4bbc95d3fc527709a79b569fbbba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db76067613f4fdd9dca",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a8187dd9fc88e85b910",
+    "url": "/static/css/main.3021bec0.chunk.css"
+  },
+  {
+    "revision": "1db76067613f4fdd9dca",
+    "url": "/static/js/2.85f54a03.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.85f54a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8187dd9fc88e85b910",
+    "url": "/static/js/main.9209f963.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "fb5b48cd0d259376d6fcd35ab8404cbf",
+    "url": "/static/media/background.fb5b48cd.gif"
+  }
+]);
