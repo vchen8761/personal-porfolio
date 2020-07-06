cf309dc8b7bbcaaedbc94fe7b2c50a362f3a28e9
vchen8761 <vchen8761@gmail.com>
2020-07-06 16:33:14 -0400

    trying to reroute url

---
+++ build/precache-manifest.8bf35a4a56b1a44a46045a3db63dbaa7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77e88d86dc4bba9643053c592f7e26c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f8b8036eebdad7b2cfa",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "18ad38501e39468a75a1",
+    "url": "/static/css/main.74556f06.chunk.css"
+  },
+  {
+    "revision": "1f8b8036eebdad7b2cfa",
+    "url": "/static/js/2.dbb3bc86.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dbb3bc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ad38501e39468a75a1",
+    "url": "/static/js/main.c5c065f8.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "fb5b48cd0d259376d6fcd35ab8404cbf",
+    "url": "/static/media/background.fb5b48cd.gif"
+  }
+]);
