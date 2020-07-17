0f7de0a5fffd85a3cec65dd73cc20c326dbd0a95
vchen8761 <vchen8761@gmail.com>
2020-07-17 08:58:34 -0400

    used media query to change display for cards

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,25 +7,23 @@ import FontAwesome from 'react-fontawesome';
 
 function NavBar() {
   return (
-    <div>
+    <div id="navigation">
       <Navbar light>
         <NavbarBrand href="/" className="mr-auto" id="brand"> Victor Chen </NavbarBrand>
+        <div id="links">
           <NavLink className="navlink" href="https://github.com/vchen8762" title="Github"> 
             <FontAwesome name="github" size="2x" style={{ '--fa-primary-color': 'red' }}/>
-            {/* <span className="tooltiptext">vchen8762 @ Github</span> */}
           </NavLink>
           <NavLink className="navlink" href="https://www.linkedin.com/in/victorchencs" title="LinkedIn">
             <FontAwesome name="linkedin" size="2x"/>
-            {/* <span className="tooltiptext">victorchencs @ Linkedin</span>  */}
           </NavLink>
           <NavLink className="navlink" href="mailto:vchen8762@gmail.com" title="Email">
             <FontAwesome name="envelope" size="2x"/>
-            {/* <span className="tooltiptext">vchen8762 @ Gmail</span> */}
           </NavLink>
           <NavLink className="navlink" href={Resume} target="_blank" title="Resume">
             <FontAwesome name="file" size="2x"/>
-            {/* <span className="tooltiptext">Resume</span> */}
           </NavLink>
+        </div>
       </Navbar>
     </div>
   );
